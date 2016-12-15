5d3c3f049e613430c64bee0916b81a5d01e8262d
Ryan Spilhaus <rspilhaus@mbbagency.com>
2016-12-15 16:51:34 -0600

    Favicons
    I always forget them

--- index.php
+++ index.php
@@ -15,6 +15,17 @@
 		
 		<title>Merry Christmas from Ryan and Bethany Spilhaus</title>
 		
+		<!--Favicons-->
+		
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="/manifest.json">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="theme-color" content="#ffffff">
+		
+		
+		
 		<!-- CSS -->		
 		<link rel="stylesheet" type="text/css" href="/build/css/style.css">
 		
